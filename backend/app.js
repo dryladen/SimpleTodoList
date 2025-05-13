3a697622195659e524c55e3f5d263c99bbae999f
dryladen <delfanladen24@gmail.com>
2025-05-13 17:59:23 +0800

    feat: configure app

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,20 @@
-const express = require("express");
 const mongoose = require("mongoose");
+const express = require("express");
 const bodyParser = require("body-parser");
 const todoRoutes = require("./routes/todos");
 const authRoutes = require("./routes/auth");
-const app = express();
-app.use(bodyParser.json());
+const morgan = require("morgan");
 
 mongoose.connect("mongodb://localhost:27017/todolist");
-const conn = mongoose.connection;
+const app = express();
 
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(morgan('dev'));
+app.use("/api/todos", todoRoutes);
+app.use("/api/auth", authRoutes);
+
+const conn = mongoose.connection;
 conn.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
@@ -16,7 +22,6 @@ conn.once("open", () => {
 conn.on("error", (err) => {
   console.error("MongoDB connection error:", err);
 });
-app.use("/api/todos", todoRoutes);
-app.use("/api/auth", authRoutes);
+
 
 app.listen(3001, () => console.log("Server running on port 3001"));

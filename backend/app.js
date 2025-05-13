278916db6f43bedfec2f88b3adfc040a7daa5e92
dryladen <delfanladen24@gmail.com>
2025-05-13 16:28:32 +0800

    feat: add new routes

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 const bodyParser = require("body-parser");
 const todoRoutes = require("./routes/todos");
-
+const authRoutes = require("./routes/auth");
 const app = express();
 app.use(bodyParser.json());
 
@@ -18,5 +17,6 @@ conn.on("error", (err) => {
   console.error("MongoDB connection error:", err);
 });
 app.use("/api/todos", todoRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(3001, () => console.log("Server running on port 3001"));

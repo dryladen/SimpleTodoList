51a8f858971a5c5f56750b00e4cacbee35b53c19
dryladen <delfanladen24@gmail.com>
2025-05-13 16:26:32 +0800

    feat: update text validation for todo

--- backend/models/Todo.js
+++ backend/models/Todo.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const TodoSchema = new mongoose.Schema(
   {
-    todo: { type: String, required: true },
+    todo: {
+      type: String,
+      required: [true, 'Todo is required']
+    },
   },
   {
     timestamps: true,

f370fddc08ccb1379098db1eed87db20afafac68
dryladen <delfanladen24@gmail.com>
2025-05-14 17:00:57 +0800

    fix: improve response messages and ensure proper user context in todo retrieval

--- backend/controllers/todoControllers.js
+++ backend/controllers/todoControllers.js
@@ -2,21 +2,24 @@ const Todo = require("../models/Todo");
 
 const getAllTodoController = async (_, res) => {
   try {
-    const todos = await Todo.find();
+    const todos = await Todo.find().populate("createdBy", "_id name")
+      .sort({ createdAt: -1 });;
     res.status(200).json({
       status: 200,
-      message: todos,
+      message: "Todos fetched successfully",
+      todos,
     });
   } catch (err) {
     res.status(500).json({ status: 500, message: err.message });
   }
 };
-const getTodoByUserController = async (_, res) => {
+const getTodoByUserController = async (req, res) => {
   try {
-    const todos = await Todo.find();
+    const todos = await Todo.find({ createdBy: req.auth._id });
     res.status(200).json({
       status: 200,
-      message: todos,
+      message: "Todos fetched successfully",
+      todos,
     });
   } catch (err) {
     res.status(500).json({ status: 500, message: err.message });
@@ -25,7 +28,11 @@ const getTodoByUserController = async (_, res) => {
 
 const createTodoController = async (req, res) => {
   try {
-    const newTodo = new Todo(req.body);
+    const { todo } = req.body;
+    const newTodo = new Todo({
+      todo,
+      createdBy: req.auth._id,
+    });
     const saved = await newTodo.save();
     if (!saved) {
       return res.status(400).json({
